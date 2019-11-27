2b496d9cb4912e2ab8d6d7f2704a2b10fcb9fdbc
marsex <53784674+marsex@users.noreply.github.com>
2019-11-27 17:15:32 -0300

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,6 @@
+import urequests
+
 def update(file_name):
-  import urequests
   updated_file=urequests.get('https://raw.githubusercontent.com/marsex/esp_update/master/'+file_name+'.py')
   file = open(file_name+".py","w")
   file.write(updated_file.text)
