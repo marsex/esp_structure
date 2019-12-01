94beb25d596e5e97aacf903cf839be94c2ec7dbb
marsex <53784674+marsex@users.noreply.github.com>
2019-12-01 18:15:01 -0300

    wp

--- update.py
+++ update.py
@@ -1,13 +1,23 @@
-def check():
-  import urequests
-  import json
+import urequests
+import json
 
-  git_url = 'https://raw.githubusercontent.com/marsex/esp_structure/master/'
-  git_sys_info=json.loads(urequests.get(git_url+'sys_info').text)
-
+def check(module_name):
+  print('git_sys_info: ', get_git_info())
+  print('esp_sys_info: ', get_esp_info())
+  if esp_sys_info[module_name] != git_sys_info[module_name]:
+    print(module_name, "status: OUTDATED")
+  else:
+    print(module_name, "status: UPDATED")
+  
+def get_esp_info():
+  global esp_sys_info
   sys_file = open('sys_info','r')
   esp_sys_info = json.loads(sys_file.read())
   sys_file.close()
-
-  print('git_sys_info: ', git_sys_info)
-  print('esp_sys_info: ', esp_sys_info)
+  return esp_sys_info
+  
+def get_git_info():
+  global git_sys_info
+  git_url = 'https://raw.githubusercontent.com/marsex/esp_structure/master/'
+  git_sys_info = json.loads(urequests.get(git_url+'sys_info').text)
+  return git_sys_info
