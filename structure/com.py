624c8d1ba0a9010971e12f2cc6432a51e167214e
marsex <marsex@protonmail.com>
2020-02-07 19:02:55 -0300

    update

--- structure/com.py
+++ structure/com.py
@@ -1,11 +1,9 @@
-import machine
 import network
 import socket
 import time
 import dht
 import ujson
-from machine import Timer
-from machine import Pin
+from machine import Timer, Pin
 import uasyncio as asyncio
 from time import sleep
 import uerrno
@@ -13,87 +11,30 @@ import uerrno
 inp_gpio=[35,34,39,36,3,21,19,18,5,17,16,4]
 out_gpio=[32,33,25,26,27,14,12,13]
 
-red=("\033[1;31;40m")
-green=("\033[1;32;40m")
-yellow=("\033[1;33;40m")
-blue=("\033[1;34;40m")
-normal=("\033[0;37;40m")
-
-hostname="192.168.0.104"
+hostname="192.168.0.107"
 USER="esp02"
-SSID="TP-LINK_56A8"
-PASSWORD="1234oooooo"
-data_in="null"
 ap_wlan=None
 st_wlan=None
 ap_socket=None
-update=-1
 
 dht1 = dht.DHT22(machine.Pin(15, Pin.IN, Pin.PULL_UP))
 dht2 = dht.DHT22(machine.Pin(22, Pin.IN, Pin.PULL_UP))
 
-machine_data = {
-  "command":"null",
-  "version":"v0.9.1.1",
-  "platform":"esp32",
-  "uptime":str(time.time()),
-  "user":USER,
-  "gpio":{
-    "input_enable":[0,0,1,0,0,0,0,0],
-    "input_state":[0,0,1,0,0,0,0,0],
-    "input_prog":[0,0,2,0,0,0,0,0],
-    "output_enable":[1,1,1,1,1,1,1,1],
-    "output_state":[0,0,0,0,0,1,0,0],
-    "output_prog":[0,0,0,0,0,0,0,0]
-  },
-  "dht1":{
-    "tmp_enable":0,
-    "tmp":0,
-    "tmp_salida":3,
-    "tmp_set":0,
-    "tmp_on_time":0,
-    "tmp_on_tick":0,
-    "tmp_off_time":0,
-    "tmp_off_tick":0,
-    "tmp_interval":0,
-    "tmp_tick":0,
-    "hmd_enable":0,
-    "hmd":0,
-    "hmd_salida":3,
-    "hmd_set":0,
-    "hmd_on_time":0,
-    "hmd_on_tick":0,
-    "hmd_off_time":0,
-    "hmd_off_tick":0,
-    "hmd_interval":0,
-    "hmd_tick":0
-  },
-  "dht2":{
-    "tmp_enable":0,
-    "tmp":0,
-    "tmp_salida":3,
-    "tmp_set":0,
-    "tmp_on_time":0,
-    "tmp_on_tick":0,
-    "tmp_off_time":0,
-    "tmp_off_tick":0,
-    "tmp_interval":0,
-    "tmp_tick":0,
-    "hmd_enable":0,
-    "hmd":0,
-    "hmd_salida":3,
-    "hmd_set":0,
-    "hmd_on_time":0,
-    "hmd_on_tick":0,
-    "hmd_off_time":0,
-    "hmd_off_tick":0,
-    "hmd_interval":0,
-    "hmd_tick":0
-  }
-}
-
+def start():
+  from structure import ble_basic as ble
+  
+  create_data()
+  
+  loop = asyncio.get_event_loop()
+  
+  loop.create_task(com())
+  loop.create_task(ble.start())
+  loop.create_task(await_dht())
+  loop.create_task(check_dht())
+  
+  loop.run_forever()
+  
 async def com():
-  print('com start')
   while True:
     input_state = [machine.Pin(i, machine.Pin.IN).value() for i in inp_gpio]
     output_state = [machine.Pin(i, machine.Pin.OUT).value() for i in out_gpio]
@@ -109,6 +50,7 @@ async def com():
     ip, port = str(address[0]), str(address[1])
     st_socket = socket.socket()
     st_socket.setblocking(False)
+
     #print('try to connect')
     try:
       #print('connecting')
@@ -116,8 +58,6 @@ async def com():
     except OSError as e:
       if e.args[0] not in [uerrno.EINPROGRESS, uerrno.ETIMEDOUT]:
         print('Error connecting', e)
-    
-    
     attempts = 2
     while attempts:
       attempts=attempts-1
@@ -141,14 +81,11 @@ async def com():
           attempts = 0
       await asyncio.sleep(0.1)
     st_socket.close()
-  print('com end')
-
-
 
 
 def parse_data(client_data):
-  try:
     print(client_data)
+  try:
     parsed_input = str(client_data[2:len(client_data)-1].replace("\'", "\""))
     print(parsed_input)
     command_json = ujson.loads(parsed_input)
@@ -176,22 +113,6 @@ def parse_data(client_data):
   except:
     print('error reading json')
 
-
-def async_dht():
-  print('async_dht')
-  loop = asyncio.get_event_loop()
-  loop.create_task(await_dht())
-  loop.run_until_complete(async_dht_done())
-
-
-def start():
-  print('async_com')
-  loop = asyncio.get_event_loop()
-  loop.create_task(com())  
-  loop.create_task(await_dht())
-  loop.create_task(check_dht())
-  loop.run_forever()
-  
 
 async def check_dht():
   while True:
@@ -234,6 +155,7 @@ async def check_dht():
       print('')
     await asyncio.sleep(1)
 
+
 async def await_dht():
   print('await_dht start')
   while True:
@@ -259,11 +181,63 @@ async def await_dht():
     await asyncio.sleep(5)
 
 
-#measure_timer=Timer(0)
-#measure_timer.init(period=5000, mode=Timer.PERIODIC, callback=lambda t:async_dht())
-
-#timeout.init(period=1000, mode=Timer.PERIODIC, callback=lambda t:http_get(hostname))
-
-
-
-
+def create_data():
+  global machine_data = {
+    "command":"null",
+    "version":"v0.9.1.1",
+    "platform":"envasadora",
+    "uptime":str(time.time()),
+    "user":USER,
+    "gpio":{
+      "input_enable":[0,0,1,0,0,0,0,0],
+      "input_state":[0,0,1,0,0,0,0,0],
+      "input_prog":[0,0,2,0,0,0,0,0],
+      "output_enable":[1,1,1,1,1,1,1,1],
+      "output_state":[0,0,0,0,0,1,0,0],
+      "output_prog":[0,0,0,0,0,0,0,0]
+    },
+    "dht1":{
+      "tmp_enable":0,
+      "tmp":0,
+      "tmp_salida":3,
+      "tmp_set":0,
+      "tmp_on_time":0,
+      "tmp_on_tick":0,
+      "tmp_off_time":0,
+      "tmp_off_tick":0,
+      "tmp_interval":0,
+      "tmp_tick":0,
+      "hmd_enable":0,
+      "hmd":0,
+      "hmd_salida":3,
+      "hmd_set":0,
+      "hmd_on_time":0,
+      "hmd_on_tick":0,
+      "hmd_off_time":0,
+      "hmd_off_tick":0,
+      "hmd_interval":0,
+      "hmd_tick":0
+    },
+    "dht2":{
+      "tmp_enable":0,
+      "tmp":0,
+      "tmp_salida":3,
+      "tmp_set":0,
+      "tmp_on_time":0,
+      "tmp_on_tick":0,
+      "tmp_off_time":0,
+      "tmp_off_tick":0,
+      "tmp_interval":0,
+      "tmp_tick":0,
+      "hmd_enable":0,
+      "hmd":0,
+      "hmd_salida":3,
+      "hmd_set":0,
+      "hmd_on_time":0,
+      "hmd_on_tick":0,
+      "hmd_off_time":0,
+      "hmd_off_tick":0,
+      "hmd_interval":0,
+      "hmd_tick":0
+    }
+  }
